857ac746d45110ec1c09d5640d384e4308ee2377
chuss1 <cal.bobbo@gmail.com>
2025-06-05 21:28:27 -0600

    created gitignore for specific files and set up the weather_gadget script

---
+++ weather_gadget/weather_gadget.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#include "./wifi_config.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println('\n');
+  Serial.println("ESP8266 booted!");
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" . . .");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(++i);
+    Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection Established");
+  Serial.print("IP address: \t");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
